8290b62adb8228e4f6bb5384cb4467d236cb5e93
UtilExe <laymasterlolz@gmail.com>
2020-10-23 14:25:17 +0200

    Implement phone validation upon adding, and ensure that a phone number that is already in use, can't be added again. It will throw an exception, and a message. + cleanup

--- src/main/java/facades/PersonFacade.java
+++ src/main/java/facades/PersonFacade.java
@@ -67,17 +67,17 @@ public class PersonFacade {
         }
     }
 
-    public PersonDTO addPerson(String email, String firstName, String lastName, String phones, String phoneDescs, String street, String additionalInfo, String zipCode, String city, String hobbies, String hobbyDescs) {
+    public PersonDTO addPerson(String email, String firstName, String lastName, String phones, String phoneDescs, String street, String additionalInfo, String zipCode, String city, String hobbies, String hobbyDescs) throws Exception {
         EntityManager em = emf.createEntityManager();
 
         Address address = new Address(street, additionalInfo);
-        //Phone phone = new Phone(phones, phoneDescs);
 
         Person realPerson = new Person(email, firstName, lastName, address);
-        //realPerson.addPhone(phone);
 
         //addP(realPerson, phones, phoneDescs, Phone.class);
         //addP(realPerson, hobbies, hobbyDescs, Hobby.class);
+        List<String> failedNumbers = new ArrayList();
+
         try {
             if (phones != null) {
 
@@ -85,10 +85,25 @@ public class PersonFacade {
                 String[] descSplit = phoneDescs.split(",");
 
                 for (int i = 0; i < aSplit.length; i++) {
+                    TypedQuery<Phone> phoneQuery = em.createQuery("SELECT p FROM Phone p WHERE p.number = :numbers", Phone.class);
+                    phoneQuery.setParameter("numbers", aSplit[i].trim());
+                    Phone phone = new Phone();
+
+                    if (!phoneQuery.getResultList().isEmpty()) {
+                        phone = (Phone) phoneQuery.getSingleResult();
+                    }
                     if (descSplit.length > i) {
-                        realPerson.addPhone(new Phone(aSplit[i].trim(), descSplit[i].trim()));
+                        if (aSplit[i].trim().equals(phone.getNumber())) {
+                            failedNumbers.add(aSplit[i].trim());
+                        } else {
+                            realPerson.addPhone(new Phone(aSplit[i].trim(), descSplit[i].trim()));
+                        }
                     } else {
-                        realPerson.addPhone(new Phone(aSplit[i].trim(), "No description"));
+                        if (aSplit[i].trim().equals(phone.getNumber())) {
+                            failedNumbers.add(aSplit[i].trim());
+                        } else {
+                            realPerson.addPhone(new Phone(aSplit[i].trim(), "No description"));
+                        }
                     }
                 }
             }
@@ -101,9 +116,9 @@ public class PersonFacade {
                     TypedQuery<Hobby> hobbyQuery = em.createQuery("SELECT h FROM Hobby h WHERE h.name = :hobbyNames", Hobby.class);
                     hobbyQuery.setParameter("hobbyNames", hSplit[i].trim().toLowerCase());
                     Hobby hobby = new Hobby();
-                    
+
                     if (!hobbyQuery.getResultList().isEmpty()) {
-                    hobby = (Hobby) hobbyQuery.getSingleResult();
+                        hobby = (Hobby) hobbyQuery.getSingleResult();
                     }
                     if (hDescSplit.length > i) {
                         if (hSplit[i].trim().toLowerCase().equals(hobby.getName())) {
@@ -129,6 +144,15 @@ public class PersonFacade {
             em.getTransaction().commit();
             return new PersonDTO(realPerson);
         } finally {
+            // The reason why it's in finally, is because want the code to still persist and continue.
+            // In an ideal world, this would be handled separately, so it doesn't hide for other exceptions.
+            if (failedNumbers.size() > 0) {
+                String failedNumberResult = "";
+                for (String failedNumber : failedNumbers) {
+                    failedNumberResult += failedNumber + ", ";
+                }
+                throw new Exception(failedNumbers.size() + " phone number(s) already in use. They were: " + failedNumberResult);
+            }
             em.close();
         }
     }
@@ -141,7 +165,8 @@ public class PersonFacade {
 
         try {
             em.getTransaction().begin();
-            Person person = em.find(Person.class, p.getId());
+            Person person = em.find(Person.class,
+                    p.getId());
             if (person == null) {
                 throw new Exception(String.format("Person with id: (%d) not found", p.getId()));
             } else {
@@ -176,16 +201,24 @@ public class PersonFacade {
 
             for (int i = 0; i < aSplit.length; i++) {
                 if (descSplit.length > i) {
-                    if (aObj.getClass().equals(Phone.class)) {
-                        person.addPhone(new Phone(aSplit[i], descSplit[i]));
-                    } else if (aObj.getClass().equals(Hobby.class)) {
-                        person.addHobby(new Hobby(aSplit[i], descSplit[i]));
+                    if (aObj.getClass().equals(Phone.class
+                    )) {
+                        person.addPhone(
+                                new Phone(aSplit[i], descSplit[i]));
+                    } else if (aObj.getClass().equals(Hobby.class
+                    )) {
+                        person.addHobby(
+                                new Hobby(aSplit[i], descSplit[i]));
                     }
                 } else {
-                    if (aObj.getClass().equals(Phone.class)) {
-                        person.addPhone(new Phone(aSplit[i], "No description"));
-                    } else if (aObj.getClass().equals(Hobby.class)) {
-                        person.addHobby(new Hobby(aSplit[i], "No description"));
+                    if (aObj.getClass().equals(Phone.class
+                    )) {
+                        person.addPhone(
+                                new Phone(aSplit[i], "No description"));
+                    } else if (aObj.getClass().equals(Hobby.class
+                    )) {
+                        person.addHobby(
+                                new Hobby(aSplit[i], "No description"));
                     }
                 }
             }
@@ -218,12 +251,14 @@ public class PersonFacade {
     public PersonsDTO getPersonByHobby(String hobbyName) throws Exception {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        TypedQuery<Hobby> hobbyQuery = em.createQuery("SELECT h FROM Hobby h WHERE h.name = :hobby", Hobby.class);
+        TypedQuery<Hobby> hobbyQuery = em.createQuery("SELECT h FROM Hobby h WHERE h.name = :hobby", Hobby.class
+        );
         hobbyQuery.setParameter("hobby", hobbyName);
         List<Hobby> hobbyList = hobbyQuery.getResultList();
         try {
             if (hobbyList != null) {
-                TypedQuery<Person> query = em.createQuery("SELECT p FROM Person p JOIN p.hobbies h WHERE h.name = :hobbies", Person.class);
+                TypedQuery<Person> query = em.createQuery("SELECT p FROM Person p JOIN p.hobbies h WHERE h.name = :hobbies", Person.class
+                );
                 query.setParameter("hobbies", hobbyName);
                 List<Person> persons = query.getResultList();
                 return new PersonsDTO(persons);
